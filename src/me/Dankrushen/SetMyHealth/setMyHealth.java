bb142917bd860387910e6343e3d159fcdfa9d742
Dankrushen <>
2015-04-27 19:40:56 -0400

    Fixed almost everything with the 2 existing commands

--- src/me/Dankrushen/SetMyHealth/setMyHealth.java
+++ src/me/Dankrushen/SetMyHealth/setMyHealth.java
@@ -32,25 +32,31 @@ public class setMyHealth extends JavaPlugin{
 				return false;
 			}
 			else if(args.length == 1){
-				double amount = Double.parseDouble(args[0]);
-				if(amount*2 <= player.getMaxHealth()){
-					player.setHealth(amount*2);
-					player.sendMessage("You have set your health to " + ChatColor.GREEN + amount + ChatColor.RESET + " hearts.");
+				if ( args[0].matches("[0-9]+") ){
+					double amount = Double.parseDouble(args[0]);
+					if(amount*2 <= player.getMaxHealth()){
+						player.setHealth(amount*2);
+						player.sendMessage("You have set your health to " + ChatColor.GREEN + amount + ChatColor.RESET + " hearts.");
+					}
+					else player.sendMessage(ChatColor.DARK_RED + "That number is too high! Your max health is " + ChatColor.DARK_GREEN + player.getMaxHealth()/2);
 				}
-				else player.sendMessage(ChatColor.DARK_RED + "That number is too high! Your max health is " + ChatColor.DARK_GREEN + player.getMaxHealth()/2);
+				else player.sendMessage(ChatColor.DARK_RED + "That is not a valid number!");
 			}
 			else if(args.length == 2){
-				@SuppressWarnings("deprecation")
-				Player target = player.getServer().getPlayer(args[0]);
-				if(target != null){
-					double amount = Double.parseDouble(args[1]);
-					if(amount*2 <= target.getMaxHealth()){
-						target.setHealth(amount*2);
-						target.sendMessage("Your health has been set to " + ChatColor.GREEN + amount + ChatColor.RESET + " hearts.");
+				if ( args[1].matches("[0-9]+") ){
+					@SuppressWarnings("deprecation")
+					Player target = player.getServer().getPlayer(args[0]);
+					if(target != null){
+						double amount = Double.parseDouble(args[1]);
+						if(amount*2 <= target.getMaxHealth()){
+							target.setHealth(amount*2);
+							target.sendMessage("Your health has been set to " + ChatColor.GREEN + amount + ChatColor.RESET + " hearts.");
+						}
+						else player.sendMessage(ChatColor.DARK_RED + "That number is too high! " + ChatColor.DARK_GREEN + args[0] + "'s" + ChatColor.DARK_RED + " max health is " + ChatColor.DARK_GREEN + target.getMaxHealth()/2);	
 					}
-					else player.sendMessage(ChatColor.DARK_RED + "That number is too high! " + ChatColor.DARK_GREEN + args[0] + "'s" + ChatColor.DARK_RED + " max health is " + ChatColor.DARK_GREEN + target.getMaxHealth()/2);	
+					else player.sendMessage(ChatColor.DARK_RED + "Could not find player \"" + ChatColor.DARK_GREEN + args[0] + ChatColor.DARK_RED + "\"");
 				}
-				else player.sendMessage(ChatColor.DARK_RED + "Could not find player \"" + ChatColor.DARK_GREEN + args[0] + ChatColor.DARK_RED + "\"");
+				else player.sendMessage(ChatColor.DARK_RED + "That is not a valid number!");
 			}
 			else if(args.length > 2){
 				player.sendMessage(ChatColor.DARK_RED + "Too many arguments!");
@@ -63,24 +69,36 @@ public class setMyHealth extends JavaPlugin{
 				return false;
 			}
 			else if(args.length == 1){
-				int amount = Integer.parseInt(args[0]);
-				player.setFoodLevel(amount*2);
-				player.sendMessage("You have set your hunger to " + ChatColor.GREEN + amount);
+				if ( args[0].matches("[0-9]+") ){
+					double amount = Double.parseDouble(args[0]);
+					Double amount2= amount*2;
+					int amountfinal = amount2.intValue();
+					if(amount <= 10){
+						player.setFoodLevel(amountfinal);
+						player.sendMessage("You have set your hunger to " + ChatColor.GREEN + amount + ChatColor.RESET + ".");
+					}
+					else player.sendMessage(ChatColor.DARK_RED + "That number is too high! Your max hunger is " + ChatColor.DARK_GREEN + "10" + ChatColor.DARK_RED + ".");
 				}
-				else player.sendMessage(ChatColor.DARK_RED + "That number is too high! Your max hunger is " + ChatColor.DARK_GREEN + "10");
+				else player.sendMessage(ChatColor.DARK_RED + "That is not a valid number!");
 			}
 			else if(args.length == 2){
-				@SuppressWarnings("deprecation")
-				Player target = player.getServer().getPlayer(args[0]);
-				if(target != null){
-					int amount = Integer.parseInt(args[1]);
-					if(amount <= 10){
-						target.setFoodLevel(amount*2);
-						target.sendMessage("Your hunger has been set to " + ChatColor.GREEN + amount);
+				if ( args[1].matches("[0-9]+") ){
+					@SuppressWarnings("deprecation")
+					Player target = player.getServer().getPlayer(args[0]);
+					if(target != null){
+						double amount = Double.parseDouble(args[1]);
+						Double amount2= amount*2;
+						int amountfinal = amount2.intValue();
+						if(amount <= 10){
+							target.setFoodLevel(amountfinal);
+							target.sendMessage("Your hunger has been set to " + ChatColor.GREEN + amount + ChatColor.RESET + ".");
+						}
+						else player.sendMessage(ChatColor.DARK_RED + "That number is too high! " + ChatColor.DARK_GREEN + args[0] + "'s" + ChatColor.DARK_RED + " max hunger is " + ChatColor.DARK_GREEN + "10" + ChatColor.DARK_RED + ".");
 					}
-					else player.sendMessage(ChatColor.DARK_RED + "That number is too high! " + ChatColor.DARK_GREEN + args[0] + "'s" + ChatColor.DARK_RED + " max hunger is " + ChatColor.DARK_GREEN + " 10");	
 				}
-				else player.sendMessage(ChatColor.DARK_RED + "Could not find player \"" + ChatColor.DARK_GREEN + args[0] + ChatColor.DARK_RED + "\"");
+				else player.sendMessage(ChatColor.DARK_RED + "That is not a valid number!");
+			}
+			else player.sendMessage(ChatColor.DARK_RED + "Could not find player \"" + ChatColor.DARK_GREEN + args[0] + ChatColor.DARK_RED + "\"");
 			}
 			else if(args.length > 2){
 				player.sendMessage(ChatColor.DARK_RED + "Too many arguments!");
